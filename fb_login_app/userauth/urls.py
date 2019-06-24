fbc3755aa00f61ca68b7ec244dac123ad84d9161
Luis Salazar <l4sh@users.noreply.github.com>
2019-06-24 22:20:52 +0000

    Add user profile view

--- fb_login_app/userauth/urls.py
+++ fb_login_app/userauth/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'userauth'
 
 urlpatterns = [
-    path('login', views.LoginView.as_view(), name='login'),
-    path('logout', views.LogoutView.as_view(), name='logout'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
--- fb_login_app/userauth/views.py
+++ fb_login_app/userauth/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView, RedirectView
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from social_django.models import UserSocialAuth
 
 
 class LoginView(TemplateView):
@@ -13,3 +16,17 @@ class LogoutView(RedirectView):
     def get(self, request, *args, **kwargs):
         logout(request)
         return super(LogoutView, self).get(request, *args, **kwargs)
+
+@method_decorator(login_required, name='get')
+class ProfileView(TemplateView):
+    """Display the user profile"""
+    template_name = 'userauth/profile.html'
+
+    def get(self, *args, **kwargs):
+        # Add facebook user id and avatar to user object
+        params = {'user': self.request.user, 'provider': 'facebook'}
+        fb_profile = get_object_or_404(UserSocialAuth, **params)
+        self.request.user.facebook_uid = fb_profile.uid
+        fb_avatar = 'https://graph.facebook.com/{}/picture?type=large'.format(fb_profile.uid)
+        self.request.user.facebook_avatar = fb_avatar
+        return super(ProfileView, self).get(*args, **kwargs)
