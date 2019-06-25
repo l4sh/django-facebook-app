71a26288bfd85fea0209db1c118ee03d2f8d9edf
Luis Salazar <l4sh@users.noreply.github.com>
2019-06-25 00:19:23 +0000

    Add redirect to profile on index

--- fb_login_app/fb_login_app/urls.py
+++ fb_login_app/fb_login_app/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, reverse_lazy
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url=reverse_lazy('userauth:profile'))),
     path('admin/', admin.site.urls),
     path('social/', include('social_django.urls', namespace='social')),
     path('auth/', include('userauth.urls', namespace='userauth')),
